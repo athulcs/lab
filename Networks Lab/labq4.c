e8c32624b0a77af86543653d235735823db45b23
athulcs <athulcs.cs@gmail.com>
2018-03-14 15:08:35 +0530

    Lab q4 changed to posix

--- Networks Lab/labq4.c
+++ Networks Lab/labq4.c
@@ -1,27 +1,26 @@
 /*Process P1 and P2 increments the value of num 5 times.Num initially=0*/
 #include<stdio.h>
-#include<pthread.h>
 #include<semaphore.h>
 #include<sys/shm.h>
 #include<unistd.h>
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 
 sem_t mutex;
 int data = 0,rcount = 0;
 
 int main()
 { sem_init(&mutex,0,1);
-  key_t key = ftok("shmfile",65);
-  int shmid = shmget(key,1024,0666|IPC_CREAT);
-  int *num = (int *) shmat(shmid,NULL,0);
+  int shm_fd = shm_open("shmfile", O_CREAT | O_RDWR, 0666);
+  ftruncate(shm_fd, 10);
+  int *num = mmap(NULL, 10, PROT_WRITE, MAP_SHARED, shm_fd, 0);
   (*num)=0;
   shmdt(num);  
   pid_t pid1;
   pid1=fork();
   if(pid1){
     for(int i=0;i<5;i++){
-      key_t key = ftok("shmfile",65);
-      int shmid = shmget(key,1024,0666|IPC_CREAT);
-      int *num = (int *) shmat(shmid,NULL,0);
       sem_wait(&mutex);
       printf("Data read from memory by P1: %d\n",*num);
       printf("Incremented value by P1: %d\n",++(*num));
@@ -33,9 +32,9 @@ int main()
   }
   else{
       for(int i=0;i<5;i++){
-        key_t key = ftok("shmfile",65);
-        int shmid = shmget(key,1024,0666|IPC_CREAT);
-        int *num = (int*) shmat(shmid,NULL,0);
+        int shm_fd = shm_open("shmfile", O_CREAT | O_RDWR, 0666);
+        ftruncate(shm_fd, 10);
+        int *num = mmap(NULL, 10, PROT_WRITE, MAP_SHARED, shm_fd, 0);
         sem_wait(&mutex);
         printf("Data read from memory by P2: %d\n",*num);
         printf("Incremented value by P2: %d\n",++(*num));
@@ -44,5 +43,6 @@ int main()
       }
 
   }
+  shm_unlink("shmfile");
   return 0;
 }
