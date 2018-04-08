041184e5cddc4762c81d65f10bf51651881eb1c0
athulcs <athulcs.cs@gmail.com>
2018-04-08 16:40:50 +0530

    Knapsack Done

--- fknap.c
+++ fknap.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
 void main(){
-	int n,m,b,i;
+	int n,m,b,i,j,tempv=0,tempw=0;
 	printf("Enter the number of objects:");
 	scanf("%d",&n);
-	int v[n],w[n],x[n];
-	float r[n];
-	printf("Enter the value and weight pairs of the objects:\n");
+	int v[n],w[n];
+	float r[n],finvalue=0;
+	printf("Enter the value and weight pairs of the objects.\n");
 	for(i=0;i<n;i++)
 		scanf("%d %d",&v[i],&w[i]);		
 	printf("Enter the capacity of knapsack:");
 	scanf("%d",&m);
-	for(i=0;i<n;i++){					//Initialising ratio
-		x[i]=0;
+
+	for(i=0;i<n;i++)					//Initialising ratio
 		r[i]=v[i]/w[i];
+	
+
+	for(i=0;i<n;i++){					//Sorting according to ratio
+		for(j=0;j<n-1;j++){
+			if(r[j]<r[j+1]){
+				tempv=v[j+1];
+				tempw=w[j+1];
+				v[j+1]=v[j];
+				w[j+1]=w[j];
+				w[j]=tempw;
+				v[j]=tempv;
+			}
+		}
 	}
-	for(i=0;i<n;i++){
-		b=m;
-		if (w[i] >b)
+
+	b=m;
+	for(i=0;i<n;i++){					//Selecting objects  by checking overflow
+
+		if (w[i] >b){
+			finvalue+=v[i]*((float)b/w[i]);
 			break;
-		x[i]=1;
-		b=b-w[i];
+		}
+		else{
+			b=b-w[i];
+			finvalue+=v[i];
+		}
+
 	}
-	if(i<=n)
-		x[i]=b/w[i];
-	printf("The answer is %d\n",x[i]);
+	
+	printf("The maximum value is %.2f\n",finvalue);
 }
